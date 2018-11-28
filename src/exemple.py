1a474dc2be3e153d510bd1c45e1f6aa045c93864
mateus.michels <michels09@hotmail.com>
2018-11-28 10:26:18 -0200

    v1.5: Add test button function to test DBconnection

--- src/exemple.py
+++ src/exemple.py
@@ -1,9 +1,9 @@
 from cfgManager import Config
 
 cfg = Config('ESghnvslK75_QDZgO3X7aqNM2Tej7JtzgsN2ogAOZzA=')
-# cfg.set_db_user('root')
-# cfg.set_db_passwd('psKgnkXDkZMVn2ojTFuEBvNg=')
-cfg.set_db_port(54364)
+cfg.set_db_user('root')
+cfg.set_db_passwd('psKkZMVn2ojTFuEBvNg=')
+cfg.set_db_port(3306)
 cfg.save()
 print(cfg.get_db_user())
 print(cfg.get_db_passwd())
--- src/tela.py
+++ src/tela.py
@@ -1,6 +1,11 @@
+import mysql.connector as mariadb
+
 import tkinter.ttk as ttk
 from io import TextIOWrapper
 from tkinter import *
+import os.path
+
+from mysql.connector import ProgrammingError
 
 import avisos
 import tela_support
@@ -8,16 +13,16 @@ from cfgManager import Config
 from cripto import Cripto
 from fileman import FileChooser
 
+w = None
+
 
 def start_gui():
     global val, w, root
     root = Tk()
+    root.protocol("WM_DELETE_WINDOW", tela_support.destroy_window)
     top = Tela(root)
     tela_support.init(root, top)
     root.mainloop()
-
-
-w = None
 
 
 def iniciar_Tela(root, *args, **kwargs):
@@ -29,12 +34,6 @@ def iniciar_Tela(root, *args, **kwargs):
     return (w, top)
 
 
-def fechar_Tela():
-    global w
-    w.destroy()
-    w = None
-
-
 class Tela:
     def __init__(self, top=None):
         self.file = TextIOWrapper
@@ -42,6 +41,7 @@ class Tela:
         self.style = ttk.Style()
         top.geometry("745x560+656+150")
         top.title("Editor CFG")
+
         # telas
         self.abas = ttk.Notebook(top, width=720)
         self.abas_db = Frame(self.abas)
@@ -62,10 +62,10 @@ class Tela:
 
         # Inputs
         self.txtDir = Entry(top)
-        self.txtDir.place(relx=0.094, rely=0.018, height=30, relwidth=0.342)
+        self.txtDir.place(relx=0.094, rely=0.018, height=30, relwidth=0.5)
 
         self.txtChave = Entry(top)
-        self.txtChave.place(relx=0.094, rely=0.09, height=30, relwidth=0.342)
+        self.txtChave.place(relx=0.094, rely=0.09, height=30, relwidth=0.5)
 
         self.txtDbUrl = Entry(self.abas_db)
         self.txtDbUrl.place(relx=0.153, rely=0.372, height=24, relwidth=0.311)
@@ -82,27 +82,41 @@ class Tela:
         self.txtDbBase = Entry(self.abas_db)
         self.txtDbBase.place(relx=0.153, rely=0.581, height=24, relwidth=0.311)
 
-        self.btnProcurar = Button(top, text='Procurar', takefocus='1')
-        self.btnProcurar.place(relx=0.444, rely=0.018, height=30, width=76)
+        self.btnProcurar = Button(top, text='Procurar', takefocus='0', command=self.procurar)
+        self.btnProcurar.place(relx=0.60, rely=0.018, height=30, width=75)
 
-        self.btnNovo = Button(top, text='Novo', takefocus='1', command=self.clk_novo)
-        self.btnNovo.place(relx=0.556, rely=0.018, height=30, width=76)
+        self.btnAcao = Button(top, text='Novo', command=self.clk_acao)
+        self.btnAcao.place(relx=0.71, rely=0.018, height=30, width=75)
 
-        self.btnAcao = Button(top, text='Carregar', command=self.clk_acao)
-        self.btnAcao.place(relx=0.444, rely=0.09, height=30, width=76)
-
-        self.btnTestar = Button(self.abas_db, text="Testar")
+        self.btnTestar = Button(self.abas_db, text="Testar", command=self.tstCon)
         self.btnTestar.place(relx=0.542, rely=0.512, height=30, width=80)
 
     def procurar(self):
         fc = FileChooser()
         self.file = fc.get_open_file_dir()
+        self.txtDir.delete(0, END)
+        self.txtDir.insert(0, self.file)
+        dir = os.path.dirname(self.file)
+        if os.path.exists(dir + '/chave.key'):
+            f = open(dir + '/chave.key', 'r')
+            self.txtChave.delete(0, END)
+            self.txtChave.insert(0, f.readline())
+            f.close()
+        self.btnAcao['text'] = 'Carregar'
 
     def clk_acao(self):
-        if self.btnAcao['text'] == 'Carregar':
+        if self.btnAcao['text'] == 'Novo':
+            self.txtChave.delete(0, END)
+            self.txtChave.insert(0, Cripto.gerar_chave())
+            self.txtDir.delete(0, END)
+            self.txtDir.insert(0, FileChooser().get_fullpath('..\\arquivos\\proj.cfg'))
+            self.btnAcao['text'] = 'Salvar'
+        elif self.btnAcao['text'] == 'Carregar':
             if self.txtChave.get() is '':
                 self.lblStatus['text'] = 'Nao foi adicionada uma chave para o arquivo'
-            # TODO: Fazer o codigo de carregamento do arquivo cfg
+            else:
+                self.ld_cfg()
+                self.btnAcao['text'] = 'Salvar'
         elif self.btnAcao['text'] == 'Salvar':
             avisos.aviso('Lembre-se de salvar sua chave!')
             if self.txtDir.get().endswith('proj.cfg'):
@@ -111,13 +125,13 @@ class Tela:
                 self.txtDir.delete(0, END)
                 self.txtDir.insert(0, self.file)
                 try:
-                    chave = open('..\\arquivos\\chave.key', 'x')
+                    chave = open(self.txtDir.get(), 'x')
                 except FileExistsError:
-                    chave = open('..\\arquivos\\chave.key', 'w')
+                    chave = open(self.txtDir.get(), 'w')
                 chave.write(self.txtChave.get())
                 chave.close()
             elif self.txtDir.get().endswith('.cfg'):
-                self.file = open(self.txtDir['text'])
+                self.file = open(self.txtDir.get())
             self.mk_cfg()
             self.cfgM.save(self.txtDir.get())
 
@@ -136,6 +150,38 @@ class Tela:
         self.cfgM.set_db_user(self.txtDbUser.get())
         self.cfgM.set_db_passwd(self.txtDbSenha.get())
         self.cfgM.set_db_name(self.txtDbBase.get())
+
+    def ld_cfg(self):
+        import cryptography.fernet
+        self.cfgM = Config(self.txtChave.get(), self.txtDir.get())
+        self.txtDbUrl.delete(0, END)
+        self.txtDbPort.delete(0, END)
+        self.txtDbUser.delete(0, END)
+        self.txtDbSenha.delete(0, END)
+        self.txtDbBase.delete(0, END)
+        try:
+            self.txtDbUrl.insert(0, self.cfgM.get_db_url())
+            self.txtDbPort.insert(0, self.cfgM.get_db_port())
+            self.txtDbUser.insert(0, self.cfgM.get_db_user())
+            self.txtDbSenha.insert(0, self.cfgM.get_db_passwd())
+            self.txtDbBase.insert(0, self.cfgM.get_db_name())
+        except cryptography.fernet.InvalidToken:
+            self.lblStatus['text'] = 'Chave invalida'
+
+    def tstCon(self):
+        end = self.txtDbUrl.get()
+        port = int(self.txtDbPort.get())
+        user = self.txtDbUser.get()
+        senha = self.txtDbSenha.get()
+        settings = {'user': user, 'password': senha, 'host': end, 'port': port, 'database': 'mysql'}
+        try:
+            con = mariadb.connect(**settings)
+            if con.is_connected():
+                self.lblStatus['text'] = 'Conexão realizada com sucesso'
+            else:
+                self.lblStatus['text'] = 'Falha ao conectar'
+        except ProgrammingError as e:
+            self.lblStatus['text'] = e
 
 
 if __name__ == '__main__':
